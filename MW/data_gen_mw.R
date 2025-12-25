51713669c4dcc2e943ef1bfe7b7f0600a0b400f1
erxc <xinyuan.eric.chen@gmail.com>
2025-12-24 22:32:52 -0600

    Create data_gen_mw.R

---
+++ MW/data_gen_mw.R
@@ -0,0 +1,49 @@
+
+# rm(list = ls())
+
+data_gen_mw <- function(n) {
+  # id
+  id <- 1:n
+  
+  # 4 covariates: 3 normal, 1 bernoulli
+  x1 <- rnorm(n)
+  x2 <- rnorm(n)
+  x3 <- rnorm(n)
+  x4 <- rbinom(n, 1, 0.5)
+  
+  # treatment assignment/probability
+  tp <- plogis(-1*x1 + 0.5*x2 - 0.25*x3 - 0.1*x4)
+  z <- rbinom(n, 1, tp)
+  
+  # principal scores/intermediate variables
+  ps <- plogis(-1 + 2*z + 1*x1 - 0.8*x2 + 0.6*x3 - 1*x4)
+  d <- rbinom(n, 1, ps)
+  
+  # outcome: continuous normal
+  sigma <- 1 # standard deviation
+  y <- 10 + 2*z - 1*d + 8*x1 + 6*x2 + 9*x3 + 7*x4 + rnorm(n = n, mean = 0, sd = sigma)
+  
+  # create 4 mutated covariates to simulate misspecification
+  x1t <- exp(0.5*x1)
+  x2t <- x2/(1+x1)
+  x3t <- (x2*x3/25+0.6)**3
+  x4t <- x3*x4/sqrt(2)
+  
+  # output data
+  data_out <- data.frame(id)
+  data_out$x1 <- x1
+  data_out$x2 <- x2
+  data_out$x3 <- x3
+  data_out$x4 <- x4
+  data_out$x1t <- x1t
+  data_out$x2t <- x2t
+  data_out$x3t <- x3t
+  data_out$x4t <- x4t
+  data_out$z <- z
+  data_out$d <- d
+  data_out$y <- y
+  
+  return(data_out)
+}
+
+# data_gen_mw(100)
